2940b1f7e14c7bb293c340ca0ec665cd505c7316
Frank Lai <frank810618@gmail.com>
2021-02-20 10:24:30 +0800

    feature: add basic codes

---
+++ src/com/frank/springdemo/BaseballCoach.java
@@ -0,0 +1,9 @@
+package com.frank.springdemo;
+
+public class BaseballCoach implements Coach {
+
+	public String getDailyPractice() {
+		return "swing the bat 100 times";
+	}
+
+}
---
+++ src/com/frank/springdemo/MyApp.java
@@ -0,0 +1,10 @@
+package com.frank.springdemo;
+
+public class MyApp {
+
+	public static void main(String args[]) {
+		Coach baseballCoach = new BaseballCoach();
+
+		System.out.println(baseballCoach.getDailyPractice());
+	}
+}
